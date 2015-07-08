e1edb54ab6d15f491298c679f1097bffc72a4f60
Roman Presnakov <roman.presnakov@aqr.com>
2015-07-08 14:42:25 -0400

    Add first project Percolation

---
+++ Assignment1-Percolation/src/main/java/Percolation.java
@@ -0,0 +1,114 @@
+/**
+ * Created by presnakovr on 7/2/2015.
+ */
+public class Percolation {
+
+    private int n;
+    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF uf2;
+
+    private boolean[] area;
+    private int topIndex;
+    private int bottomIndex;
+
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw  new IllegalArgumentException();
+        }
+
+        this.n = n;
+
+        uf = new WeightedQuickUnionUF(n * n + 2);
+        area = new boolean[n * n];
+        for (int i = 0; i < n*n; i++) {
+            area[i] = false;
+        }
+
+        topIndex = n * n;
+        bottomIndex = n * n + 1;
+
+        uf2 = new WeightedQuickUnionUF(n * n + 1);
+    }
+
+    // open site (row i, column j) if it is not open already
+    public void open(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        area[getIndex(i, j)] = true;
+        //check if side in the top line
+        if (i == 1) {
+            uf.union(getIndex(i, j), topIndex);
+            uf2.union(getIndex(i, j), topIndex);
+        }
+
+        if (i == n) {
+            uf.union(getIndex(i, j), bottomIndex);
+        }
+
+        checkAndConnectNeighbours(i, j, i, j - 1);
+        checkAndConnectNeighbours(i, j, i, j + 1);
+        checkAndConnectNeighbours(i, j, i - 1, j);
+        checkAndConnectNeighbours(i, j, i + 1, j);
+    }
+
+    private void checkAndConnectNeighbours(int i, int j, int ii, int jj) {
+        if (ii >= 1 && ii <= n && jj >= 1 && jj <= n && isOpen(ii, jj)) {
+            uf.union(getIndex(i, j), getIndex(ii, jj));
+            uf2.union(getIndex(i, j), getIndex(ii, jj));
+        }
+    }
+
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return area[getIndex(i, j)];
+    }
+
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        if (i < 1 || i > n || j < 1 || j > n) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (!isOpen(i, j)) {
+            return false;
+        }
+
+        return uf.connected(getIndex(i, j), topIndex) && uf2.connected(getIndex(i, j), topIndex);
+    }
+
+    // does the system percolate?
+    public boolean percolates()  {
+        return uf.connected(topIndex, bottomIndex);
+    }
+
+    private int getIndex(int i, int j) {
+        return n * (i - 1) + (j  -1);
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        Percolation perc = new Percolation(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+
+            perc.open(p, q);
+            StdOut.println(perc.isFull(p, q));
+            StdOut.println(perc.percolates());
+
+            if (p == 5 && q == 4) {
+                break;
+            }
+        }
+
+        StdOut.println(perc.isFull(1, 1));
+    }
+}
---
+++ Assignment1-Percolation/src/main/java/PercolationStats.java
@@ -0,0 +1,107 @@
+/**
+ * Created by presnakovr on 7/2/2015.
+ */
+public class PercolationStats {
+
+    private double[] experiment;
+
+    private int n;
+    private int t;
+
+    private double mean = 0;
+    private double stddev = 0;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int n, int t) {
+        if (n <= 0) {
+            throw  new IllegalArgumentException();
+        }
+        if (t <= 0) {
+            throw  new IllegalArgumentException();
+        }
+
+        this.n = n;
+        this.t = t;
+
+        experiment = new double[t];
+
+        for (int ind = 0; ind < t; ind++) {
+            doExperiment(ind);
+        }
+    }
+
+    private void doExperiment(int index) {
+        Percolation percolation = new Percolation(n);
+
+        int count = 0;
+
+        while (!percolation.percolates() && count < n * n) {
+            count++;
+
+            int i = StdRandom.uniform(1, n + 1);
+            int j = StdRandom.uniform(1, n + 1);
+
+            while (percolation.isOpen(i, j)) {
+                i = StdRandom.uniform(1, n + 1);
+                j = StdRandom.uniform(1, n + 1);
+            }
+            percolation.open(i, j);
+        }
+
+        experiment[index] = (double) count / (n * n);
+
+        //draw percolation
+        //PercolationVisualizer.draw(percolation, n);
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        mean = StdStats.mean(experiment);
+        return mean;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        stddev = StdStats.stddev(experiment);
+        return stddev;
+    }
+
+    // low  endpoint of 95% confidence interval
+    public double confidenceLo() {
+        checkMeanAndStddev();
+
+        return mean - 1.96 * stddev / Math.sqrt(t);
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        checkMeanAndStddev();
+
+        return mean + 1.96 * stddev / Math.sqrt(t);
+    }
+
+    private void checkMeanAndStddev() {
+        if (mean == 0) {
+            mean = mean();
+        }
+
+        if (stddev == 0) {
+            stddev = stddev();
+        }
+    }
+
+    // test client (described below)
+    public static void main(String[] args) {
+//        args[0] = "320";
+//        args[1] = "100";
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]),
+                Integer.parseInt(args[1]));
+
+        StdOut.printf("%-23s = ", "mean");
+        StdOut.println(stats.mean());
+        StdOut.printf("%-23s = ", "stddev");
+        StdOut.println(stats.stddev());
+        StdOut.printf("%-23s = ", "95% confidence interval");
+        StdOut.println(stats.confidenceLo() + ", " + stats.confidenceHi());
+    }
+}
