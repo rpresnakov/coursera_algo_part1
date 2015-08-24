858726bd968b0b95cf751ede9f2183bfaaf87fcf
presnakov (home) <roman.presnakov@gmail.com>
2015-08-24 03:47:48 -0400

    first version of solution for assignment 4

---
+++ Assignment4-8Puzzle/src/main/java/Board.java
@@ -0,0 +1,219 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.Arrays;
+
+/**
+ * Created by Roman on 8/23/2015.
+ */
+public class Board {
+
+    private int[][] blocks;
+    private int N;
+
+    private class Coordinate {
+        private int i;
+        private int j;
+    }
+
+    public Board(int[][] blocks) {
+        this.blocks = blocks;
+        N = blocks.length;
+    }
+
+    public int dimension() {
+        return N;
+    }
+
+    public int hamming() {
+        int count = 0;
+
+        // check that all numbers are in ascending order in place
+        for (int k = 1; k < N*N; k++) {
+            int i = (k - 1) / N;
+            int j = k - i * N - 1;
+            if (blocks[i][j] != k) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int manhattan() {
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int k = blocks[i][j];
+                if (k == 0) {
+                    continue;
+                }
+
+                int ni = (k - 1) / N;
+                int nj = k - ni * N - 1;
+
+                int di = ni - i;
+                if (di < 0) {
+                    di *= -1;
+                }
+                int dj = nj - j;
+                if (dj < 0) {
+                    dj *= -1;
+                }
+
+                count = count + di + dj;
+            }
+        }
+
+        return count;
+    }
+
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+
+    public Board twin() {
+        return this;
+    }
+
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+
+        if (y == null) {
+            return false;
+        }
+
+        if (y instanceof Board) {
+            if (this.toString().equals(y.toString())) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Iterable<Board> neighbors() {
+        Stack<Board> neighbors = new Stack<>();
+
+        Coordinate zeroElement = findZero(blocks);
+        Coordinate nonZeroElement = new Coordinate();
+        int[][] result;
+
+        // left
+        nonZeroElement.i = zeroElement.i;
+        nonZeroElement.j = zeroElement.j - 1;
+        addNeighbor(neighbors, blocks, zeroElement, nonZeroElement);
+        // right
+        nonZeroElement.i = zeroElement.i;
+        nonZeroElement.j = zeroElement.j + 1;
+        addNeighbor(neighbors, blocks, zeroElement, nonZeroElement);
+        // up
+        nonZeroElement.i = zeroElement.i - 1;
+        nonZeroElement.j = zeroElement.j;
+        addNeighbor(neighbors, blocks, zeroElement, nonZeroElement);
+        // down
+        nonZeroElement.i = zeroElement.i + 1;
+        nonZeroElement.j = zeroElement.j;
+        addNeighbor(neighbors, blocks, zeroElement, nonZeroElement);
+
+        return neighbors;
+    }
+
+    /**
+     * Find zero element.
+     * @param blocks
+     * @return
+     */
+    private Coordinate findZero(final int[][] blocks) {
+        Coordinate zero = new Coordinate();
+
+        int i = 0;
+        int j = 0;
+
+        // find zero element
+        while (blocks[i][j] != 0 && i < N) {
+            if (j == N - 1) {
+                i++;
+                j = 0;
+            } else {
+                j++;
+            }
+        }
+
+        zero.i = i;
+        zero.j = j;
+
+        return zero;
+    }
+
+    /**
+     * Build neighbor boar with exchanging zero and nonzero elements.
+     * @param original
+     * @param zeroElement
+     * @param nonZeroElement
+     * @return
+     */
+    private void addNeighbor(
+            final Stack<Board> neighbors, final int[][] original, final Coordinate zeroElement, final Coordinate nonZeroElement) {
+        if (nonZeroElement.i < 0 || nonZeroElement.j < 0
+                || nonZeroElement.i >= N || nonZeroElement.j >= N) {
+            return;
+        }
+
+        // copying array
+        int[][] result = new int[original.length][];
+        for (int i = 0; i < original.length; i++) {
+            result[i] = Arrays.copyOf(original[i], original.length);
+        }
+
+        // exchanging elements;
+        result[zeroElement.i][zeroElement.j] = result[nonZeroElement.i][nonZeroElement.j];
+        result[nonZeroElement.i][nonZeroElement.j] = 0;
+
+        neighbors.push(new Board(result));
+    }
+
+    public String toString() {
+        StringBuffer str = new StringBuffer();
+        str.append(N).append("\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                str.append(blocks[i][j]);
+                if (j != (N - 1)) {
+                    str.append(" ");
+                } else {
+                    str.append("\n");
+                }
+            }
+        }
+        return str.toString();
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(blocks);
+        Board initial1 = new Board(blocks);
+
+        StdOut.println(initial1.equals(initial));
+        StdOut.println(initial.isGoal());
+        StdOut.println(initial);
+
+        for (Board neighbor : initial.neighbors()) {
+            StdOut.println(neighbor);
+        }
+
+        StdOut.println(initial.hamming());
+        StdOut.println(initial.manhattan());
+    }
+}
---
+++ Assignment4-8Puzzle/src/main/java/Solver.java
@@ -0,0 +1,117 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.MinPQ;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ * Created by Roman on 8/23/2015.
+ */
+public class Solver {
+
+    private class Node implements Comparable<Node> {
+
+        private Board board;
+        int moves;
+        private Node previous;
+
+        Node (Board board, int moves, Node previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            int thisM = this.board.manhattan();
+            int thatM = o.board.manhattan();
+
+            return (thisM + moves) - (thatM + o.moves);
+        }
+    }
+
+    private MinPQ<Node> prQueue = new MinPQ<>();
+    private boolean solvable = false;
+    private Node goalNode = null;
+
+    public Solver(Board initial) {
+        if (initial == null) {
+            throw new NullPointerException();
+        }
+
+        solve(initial);
+    }
+
+    private void solve(Board initial) {
+        Node searchNode = new Node(initial, 0, null);
+        prQueue.insert(searchNode);
+
+        int moves = 0;
+        while (!prQueue.min().board.isGoal()) {
+            moves++;
+            searchNode = prQueue.delMin();
+            for (Board neighbor : searchNode.board.neighbors()) {
+                if (searchNode.previous == null ||
+                        !neighbor.equals(searchNode.previous.board)) {
+                    prQueue.insert(new Node(neighbor, moves, searchNode));
+                }
+            }
+        }
+
+        solvable = true;
+        goalNode = prQueue.delMin();
+        prQueue = null;
+    }
+
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    public int moves() {
+        if (!isSolvable()) {
+            return -1;
+        } else {
+            return goalNode.moves;
+        }
+    }
+
+    public Iterable<Board> solution() {
+        if (!isSolvable()) {
+            return null;
+        }
+        Stack<Board> solution = new Stack<>();
+        solution.push(goalNode.board);
+        Node previousNode = goalNode.previous;
+        while (previousNode != null) {
+            solution.push(previousNode.board);
+            previousNode = previousNode.previous;
+        }
+
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int N = in.readInt();
+        int[][] blocks = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(blocks);
+
+        //solve the puzzle
+        Solver solver = new Solver(initial);
+
+        //print solution to standard output
+        if (!solver.isSolvable()) {
+            StdOut.println("No solution possible");
+        } else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution()) {
+                StdOut.println(board);
+            }
+        }
+    }
+}
